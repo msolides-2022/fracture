0c1d44e47588b6892237dd00adcfa1a3fce3c6c5
Corrado Maurini <corrado.maurini@sorbonne-universite.fr>
2022-09-07 17:20:15 +0200

    updating install instructions

--- INSTALL.md
+++ INSTALL.md
@@ -1,55 +1,38 @@
 # How to install FEniCS
 
-## On your laptop
+Instructions on how to install FEniCS-dolfinx are available https://fenicsproject.org/download/.
 
-Instructions on how to install FEniCS are available https://fenicsproject.org/download/.
-You also need a working `python3` installation with several additional packages.
+We suggest and support the installation method using anaconda. Anaconda is a useful package manager for python. 
 
-**Except for Ubuntu distributions (see below)**, we suggest at first to try to use Anaconda, which has an advanced package manager that can provide you without effort most of the software that you need. Install the Anaconda version based on *python3*.
+The following install instruction work only on Linux on Macos. If you have Windows, see below.
 
-### On ubuntu (preferred installation)
+1. Install Anaconda from https://www.anaconda.com/products/distribution. 
+    - If you are on Macosx: choose the install form your platform:
+        - Intel processors: https://repo.anaconda.com/archive/Anaconda3-2022.05-MacOSX-x86_64.pkg
+        - M1 processors: https://repo.anaconda.com/archive/Anaconda3-2022.05-MacOSX-arm64.pkg
 
-Install basic Python 3 packages
+2. Open a new terminal and go to the directory containing the file `fenicsx-0.5.0.yaml` you find in this repository
 
-```
-sudo apt update
-sudo apt upgrade
-sudo apt install black cython3 jupyter-core jupyter-client jupyter-notebook pylint python3-autopep8 python3-numpy python3-flake8 python3-h5py python3-jupyter-console python3-matplotlib python3-mpi4py python3-notebook python3-petsc4py python3-pytest python3-requests python3-scipy python3-slepc4py python3-skimage python3-sphinx python3-sympy spyder pelican jupyter-nbconvert jupyter-qtconsole python3-qtconsole
-```
+3. Create a new conda enviroment from the file `fenicsx-0.5.0.yaml.
+    ```conda env create -n fenicsx-test --file fenicsx-0.5.0.yml```
 
-Then install the FEniCS specific packages
+4. You have now installed fenics in the conda environment `fenicsx-0.5.0`. To use it you must activate the enviroment with the following command
+    ```
+    conda activate fenicsx-0.5.0
+    ```
 
-```
-sudo apt-get install software-properties-common
-sudo add-apt-repository ppa:fenics-packages/fenics
-sudo apt-get update
-sudo apt-get install --no-install-recommends fenics
-```
+After the first installation, you need only step 4 above (`conda activate fenicsx-0.5.0`) to use fenicsx on a terminal.
+
+You can find further help on conda [here](https://docs.conda.io/projects/conda/en/latest/_downloads/843d9e0198f2a193a3484886fa28163c/conda-cheatsheet.pdf)
 
 ### On windows
 
-FEniCS is not distributed for Windows boxes. For Windows 10, the preferred option is the [Windows subsystem for linux (WSL)](https://docs.microsoft.com/en-us/windows/wsl/install-win10): install the Ubuntu distribution, then refer to the section above.
+FEniCS is not distributed for Windows boxes. For Windows 10, the preferred option is the [Windows subsystem for linux (WSL)](https://docs.microsoft.com/en-us/windows/wsl/install-win10).
+Install the Ubuntu distribution as WSL, then refer to the section above inside the Ubuntu WSL
 
 If you cannot install the WSL, you will need to create Ubuntu virtual machine. Get in touch with your instructors in that case.
-Then in
-side the ubuntu virtual machine
+Then inside the ubuntu virtual machine follows the instruction for conda installation
 
-```
-sudo apt-get install software-properties-common
-sudo add-apt-repository ppa:fenics-packages/fenics
-sudo apt-get update && upgrade
-sudo apt-get install fenics
-sudo apt install python3 python3-pip ipython3 jupyter-core jupyter
-sudo pip3 install jupyterlab
-```
-
-To launch a notebook:
-
-```
-jupyter lab --no-browser
-```
-
-And then open the http address indicated in the ubuntu terminal in the browser of your windows system. 
 
 ## Cloud-based  Google colab installations
 You can run python programs jupyter notebooks and FEniCS on online servers. The basic service is free and can be a solution if all other installation systems fail.
@@ -63,59 +46,6 @@ We suggest to use this only as emergency solution
 
 * *disadvantages:* It can be slow (especially Azure, colab seems better). You can use only jupyter notebooks, you share your date with google or microsoft, you do not a full control of the system, you need to be online with a good network connection.
 
-
-### Using docker virtual machines (suggested method if others fail) :
-This works on windows, linux, macos
-1. First download docker https://www.docker.com/get-started 
-2. On the terminal (this will share the current directory with the directory `/home/fenics/shared` in the virtual machine):
-    ```
-    docker run --name notebook -w /home/fenics -v $(pwd):/home/fenics/shared -d -p 127.0.0.1:8888:8888 cmaurini/fenics-stable-20.04 'jupyter-notebook --ip=0.0.0.0 --allow-root'
-3. At the terminal type ` docker logs notebook ` and open the http address indicated in the output in your browser. This will give you a jupyter notebook working in the virtual machine. 
-
-See also https://fenicsproject.org/download/ on the docker section and https://fenics-containers.readthedocs.io/en/latest/index.html for further issue
-
-
-* If you have an error like 
-    ```
-    docker: Error response from daemon: Conflict. The container name "/notebook" is already in use by container "db4b0b976ae09df4156680b9b667291e39237f8c457eee735d5f7de7fff20459". You have to remove (or rename) that container to be able to reuse that name.
-    ```
-    execute at the terminal `docker rm -f notebook` and try again. 
-* If you want a terminal inside the virtual machine without the notebook interface: 
-    ```
-    docker run -ti  -v $(pwd):/home/fenics/shared -w /home/fenics/shared cmaurini/fenics-stable-20.04
-    ```
-### Anaconda install (Mac and Linux)
-
-This procedure could work on Mac (but it is not guaranteed). We do not suggest to follows this on linux.
-
-First install Anaconda, following the instruction here  `https://docs.continuum.io/anaconda/install/`.
-
-Then run following commands in your terminal (the file `fenicsproject.yml` must be in your directory):
-<!---
-conda env create -f fenicsproject.yml
--->
-
-```
-conda config --add channels conda-forge
-conda config --set channel_priority strict
-conda create -n fenicsproject
-source activate fenicsproject
-conda install fenics mshr
-```
-
-The above commands will create a virtual environment called `fenicsproject`, in which the FEniCS library and other useful packages will be installed. This avoids conflicts between various packages. This means that  **you need to activate the `fenicsproject` environment** when you install additional packages or perform FEniCS computations. If the environment is activated, your terminal prompt will display `(fenicsproject)`, like so:
-
-```
-(fenicsproject) maurini@maurinis-MacBook-Pro:~$
-```
-
-We suggest also to install other useful packages 
-
-```
-conda install gmsh pip jupyter spyder matplotlib scipy vtkplotter meshio
-```
-
-If you need to install further packages, just type `conda install packagename` (preferred) or `pip3 install packagename`.
 ## How to test the installation
 
 Run following commands in your terminal:
@@ -127,29 +57,32 @@ python3 demo_poisson.py
 or copy and paste this code in the notebook and exectute the cell
 
 ```
-import dolfin 
-import mshr
+import numpy as np
 import ufl
-print("You are using FEniCS version {:s}".format(dolfin.__version__))
-mesh = dolfin.UnitSquareMesh(64, 64)
-V = dolfin.FunctionSpace(mesh, "Lagrange", 1)
-# Define Dirichlet boundary (x = 0 or x = 1)
-def boundary(x):
-    return x[0] < dolfin.DOLFIN_EPS or x[0] > 1.0 - dolfin.DOLFIN_EPS
-# Define boundary condition
-u0 = dolfin.Constant(0.0)
-bc = dolfin.DirichletBC(V, u0, boundary)
-# Define variational problem
+from dolfinx import fem, io, mesh, plot
+from ufl import ds, dx, grad, inner
+from mpi4py import MPI
+from petsc4py.PETSc import ScalarType
+msh = mesh.create_rectangle(comm=MPI.COMM_WORLD,
+                            points=((0.0, 0.0), (2.0, 1.0)), n=(32, 16),
+                            cell_type=mesh.CellType.triangle,)
+V = fem.FunctionSpace(msh, ("Lagrange", 1))
+facets = mesh.locate_entities_boundary(msh, dim=1,
+                                       marker=lambda x: np.logical_or(np.isclose(x[0], 0.0),
+                                                                      np.isclose(x[0], 2.0)))
+dofs = fem.locate_dofs_topological(V=V, entity_dim=1, entities=facets)
+bc = fem.dirichletbc(value=ScalarType(0), dofs=dofs, V=V)
 u = ufl.TrialFunction(V)
 v = ufl.TestFunction(V)
-f = dolfin.Expression("10 * exp(-(pow(x[0] - 0.5, 2) + pow(x[1] - 0.5, 2)) / 0.02)", degree=2)
-g = dolfin.Expression("sin(5*x[0])", degree=2)
-a = ufl.inner(ufl.grad(u), ufl.grad(v)) * ufl.dx
-L = f * v * ufl.dx + g * v * ufl.ds
-# Compute solution
-u = dolfin.Function(V)
-A, b = dolfin.assemble_system(a,L,[bc])
-dolfin.solve(A,u.vector(),b) 
-dolfin.plot(u)
+x = ufl.SpatialCoordinate(msh)
+f = 10 * ufl.exp(-((x[0] - 0.5) ** 2 + (x[1] - 0.5) ** 2) / 0.02)
+g = ufl.sin(5 * x[0])
+a = inner(grad(u), grad(v)) * dx
+L = inner(f, v) * dx + inner(g, v) * ds
+problem = fem.petsc.LinearProblem(a, L, bcs=[bc], petsc_options={"ksp_type": "preonly", "pc_type": "lu"})
+uh = problem.solve()
+with io.XDMFFile(msh.comm, "out_poisson/poisson.xdmf", "w") as file:
+    file.write_mesh(msh)
+    file.write_function(uh)
 ```
 
--- demo_poisson.py
+++ demo_poisson.py
@@ -1,190 +1,151 @@
-# 
-# .. _demo_poisson_equation:
-# 
-# Poisson equation
-# ================
-# 
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.13.6
+# ---
+
+# # Poisson equation
+#
 # This demo is implemented in a single Python file,
-# :download:`demo_poisson.py`, which contains both the variational forms
-# and the solver.
-# 
-# This demo illustrates how to:
-# 
-# * Solve a linear partial differential equation
-# * Create and apply Dirichlet boundary conditions
-# * Define Expressions
-# * Define a FunctionSpace
-# * Create a SubDomain
-# 
-# The solution for :math:`u` in this demo will look as follows:
-# 
-# .. image:: poisson_u.png
-#    :scale: 75 %
-# 
-# 
-# Equation and problem definition
-# -------------------------------
-# 
-# The Poisson equation is the canonical elliptic partial differential
-# equation.  For a domain :math:`\Omega \subset \mathbb{R}^n` with
-# boundary :math:`\partial \Omega = \Gamma_{D} \cup \Gamma_{N}`, the
-# Poisson equation with particular boundary conditions reads:
-# 
-# .. math::
-#    - \nabla^{2} u &= f \quad {\rm in} \ \Omega, \\
-#                 u &= 0 \quad {\rm on} \ \Gamma_{D}, \\
-#                 \nabla u \cdot n &= g \quad {\rm on} \ \Gamma_{N}. \\
-# 
-# Here, :math:`f` and :math:`g` are input data and :math:`n` denotes the
-# outward directed boundary normal. The most standard variational form
-# of Poisson equation reads: find :math:`u \in V` such that
-# 
-# .. math::
-#    a(u, v) = L(v) \quad \forall \ v \in V,
-# 
-# where :math:`V` is a suitable function space and
-# 
-# .. math::
-#    a(u, v) &= \int_{\Omega} \nabla u \cdot \nabla v \, {\rm d} x, \\
-#    L(v)    &= \int_{\Omega} f v \, {\rm d} x
-#    + \int_{\Gamma_{N}} g v \, {\rm d} s.
-# 
-# The expression :math:`a(u, v)` is the bilinear form and :math:`L(v)`
-# is the linear form. It is assumed that all functions in :math:`V`
-# satisfy the Dirichlet boundary conditions (:math:`u = 0 \ {\rm on} \
-# \Gamma_{D}`).
-# 
-# In this demo, we shall consider the following definitions of the input
-# functions, the domain, and the boundaries:
-# 
-# * :math:`\Omega = [0,1] \times [0,1]` (a unit square)
-# * :math:`\Gamma_{D} = \{(0, y) \cup (1, y) \subset \partial \Omega\}`
-#   (Dirichlet boundary)
-# * :math:`\Gamma_{N} = \{(x, 0) \cup (x, 1) \subset \partial \Omega\}`
-#   (Neumann boundary)
-# * :math:`g = \sin(5x)` (normal derivative)
-# * :math:`f = 10\exp(-((x - 0.5)^2 + (y - 0.5)^2) / 0.02)` (source
-#   term)
-# 
-# 
-# Implementation
-# --------------
-# 
-# This description goes through the implementation (in
-# :download:`demo_poisson.py`) of a solver for the above described
-# Poisson equation step-by-step.
-# 
-# First, the :py:mod:`dolfin` module is imported: ::
+# {download}`demo_poisson.py`, which contains both the variational forms
+# and the solver. It illustrates how to:
+#
+# - Solve a linear partial differential equation
+# - Define a {py:class}`FunctionSpace <dolfinx.fem.FunctionSpace>`
+# - Create and apply Dirichlet boundary conditions
+#
+# ## Equation and problem definition
+#
+# For a domain $\Omega \subset \mathbb{R}^n$ with boundary $\partial
+# \Omega = \Gamma_{D} \cup \Gamma_{N}$, the Poisson equation with
+# particular boundary conditions reads:
+#
+# $$
+# \begin{align}
+# - \nabla^{2} u &= f \quad {\rm in} \ \Omega, \\
+# u &= 0 \quad {\rm on} \ \Gamma_{D}, \\
+# \nabla u \cdot n &= g \quad {\rm on} \ \Gamma_{N}. \\
+# \end{align}
+# $$
+#
+# where $f$ and $g$ are input data and $n$ denotes the outward directed
+# boundary normal. The variational problem reads: find $u \in V$ such
+# that
+#
+# $$
+# a(u, v) = L(v) \quad \forall \ v \in V,
+# $$
+#
+# where $V$ is a suitable function space and
+#
+# $$
+# \begin{align}
+# a(u, v) &:= \int_{\Omega} \nabla u \cdot \nabla v \, {\rm d} x, \\
+# L(v)    &:= \int_{\Omega} f v \, {\rm d} x + \int_{\Gamma_{N}} g v \, {\rm d} s.
+# \end{align}
+# $$
+#
+# The expression $a(u, v)$ is the bilinear form and $L(v)$
+# is the linear form. It is assumed that all functions in $V$
+# satisfy the Dirichlet boundary conditions ($u = 0 \ {\rm on} \
+# \Gamma_{D}$).
+#
+# In this demo we consider:
+#
+# - $\Omega = [0,2] \times [0,1]$ (a rectangle)
+# - $\Gamma_{D} = \{(0, y) \cup (1, y) \subset \partial \Omega\}$
+# - $\Gamma_{N} = \{(x, 0) \cup (x, 1) \subset \partial \Omega\}$
+# - $g = \sin(5x)$
+# - $f = 10\exp(-((x - 0.5)^2 + (y - 0.5)^2) / 0.02)$
+#
+# ## Implementation
+#
+# The modules that will be used are imported:
 
-from dolfin import *
+# +
+import numpy as np
 
-# We begin by defining a mesh of the domain and a finite element
-# function space :math:`V` relative to this mesh. As the unit square is
-# a very standard domain, we can use a built-in mesh provided by the
-# class :py:class:`UnitSquareMesh <dolfin.cpp.UnitSquareMesh>`. In order
-# to create a mesh consisting of 32 x 32 squares with each square
-# divided into two triangles, we do as follows ::
+import ufl
+from dolfinx import fem, io, mesh, plot
+from ufl import ds, dx, grad, inner
 
-# Create mesh and define function space
-mesh = UnitSquareMesh(32, 32)
-V = FunctionSpace(mesh, "Lagrange", 1)
+from mpi4py import MPI
+from petsc4py.PETSc import ScalarType
+# -
 
-# The second argument to :py:class:`FunctionSpace
-# <dolfin.functions.functionspace.FunctionSpace>` is the finite element
-# family, while the third argument specifies the polynomial
-# degree. Thus, in this case, our space ``V`` consists of first-order,
-# continuous Lagrange finite element functions (or in order words,
-# continuous piecewise linear polynomials).
-# 
-# Next, we want to consider the Dirichlet boundary condition. A simple
-# Python function, returning a boolean, can be used to define the
-# subdomain for the Dirichlet boundary condition (:math:`\Gamma_D`). The
-# function should return ``True`` for those points inside the subdomain
-# and ``False`` for the points outside. In our case, we want to say that
-# the points :math:`(x, y)` such that :math:`x = 0` or :math:`x = 1` are
-# inside on the inside of :math:`\Gamma_D`. (Note that because of
-# rounding-off errors, it is often wise to instead specify :math:`x <
-# \epsilon` or :math:`x > 1 - \epsilon` where :math:`\epsilon` is a
-# small number (such as machine precision).) ::
+# We begin by using {py:func}`create_rectangle
+# <dolfinx.mesh.create_rectangle>` to create a rectangular
+# {py:class}`Mesh <dolfinx.mesh.Mesh>` of the domain, and creating a
+# finite element {py:class}`FunctionSpace <dolfinx.fem.FunctionSpace>`
+# $V$ on the mesh.
 
-# Define Dirichlet boundary (x = 0 or x = 1)
-def boundary(x):
-    return x[0] < DOLFIN_EPS or x[0] > 1.0 - DOLFIN_EPS
+# +
+msh = mesh.create_rectangle(comm=MPI.COMM_WORLD,
+                            points=((0.0, 0.0), (2.0, 1.0)), n=(32, 16),
+                            cell_type=mesh.CellType.triangle,)
+V = fem.FunctionSpace(msh, ("Lagrange", 1))
+# -
 
-# Now, the Dirichlet boundary condition can be created using the class
-# :py:class:`DirichletBC <dolfin.fem.bcs.DirichletBC>`. A
-# :py:class:`DirichletBC <dolfin.fem.bcs.DirichletBC>` takes three
-# arguments: the function space the boundary condition applies to, the
-# value of the boundary condition, and the part of the boundary on which
-# the condition applies. In our example, the function space is ``V``,
-# the value of the boundary condition (0.0) can represented using a
-# :py:class:`Constant <dolfin.functions.constant.Constant>` and the
-# Dirichlet boundary is defined immediately above. The definition of the
-# Dirichlet boundary condition then looks as follows: ::
+# The second argument to {py:class}`FunctionSpace
+# <dolfinx.fem.FunctionSpace>` is a tuple consisting of `(family,
+# degree)`, where `family` is the finite element family, and `degree`
+# specifies the polynomial degree. in this case `V` consists of
+# first-order, continuous Lagrange finite element functions.
+#
+# Next, we locate the mesh facets that lie on the boundary $\Gamma_D$.
+# We do this using using {py:func}`locate_entities_boundary
+# <dolfinx.mesh.locate_entities_boundary>` and providing  a marker
+# function that returns `True` for points `x` on the boundary and
+# `False` otherwise.
 
-# Define boundary condition
-u0 = Constant(0.0)
-bc = DirichletBC(V, u0, boundary)
+facets = mesh.locate_entities_boundary(msh, dim=1,
+                                       marker=lambda x: np.logical_or(np.isclose(x[0], 0.0),
+                                                                      np.isclose(x[0], 2.0)))
 
-# Next, we want to express the variational problem.  First, we need to
-# specify the trial function :math:`u` and the test function :math:`v`,
-# both living in the function space :math:`V`. We do this by defining a
-# :py:class:`TrialFunction <dolfin.functions.function.TrialFunction>`
-# and a :py:class:`TestFunction
-# <dolfin.functions.function.TrialFunction>` on the previously defined
-# :py:class:`FunctionSpace
-# <dolfin.functions.functionspace.FunctionSpace>` ``V``.
-# 
-# Further, the source :math:`f` and the boundary normal derivative
-# :math:`g` are involved in the variational forms, and hence we must
-# specify these. Both :math:`f` and :math:`g` are given by simple
-# mathematical formulas, and can be easily declared using the
-# :py:class:`Expression <dolfin.functions.expression.Expression>` class.
-# Note that the strings defining ``f`` and ``g`` use C++ syntax since,
-# for efficiency, DOLFIN will generate and compile C++ code for these
-# expressions at run-time.
-# 
-# With these ingredients, we can write down the bilinear form ``a`` and
-# the linear form ``L`` (using UFL operators). In summary, this reads ::
+# We now find the degrees-of-freedom that are associated with the
+# boundary facets using {py:func}`locate_dofs_topological
+# <dolfinx.fem.locate_dofs_topological>`
 
-# Define variational problem
-u = TrialFunction(V)
-v = TestFunction(V)
-f = Expression("10*exp(-(pow(x[0] - 0.5, 2) + pow(x[1] - 0.5, 2)) / 0.02)", degree=2)
-g = Expression("sin(5*x[0])", degree=2)
-a = inner(grad(u), grad(v))*dx
-L = f*v*dx + g*v*ds
+dofs = fem.locate_dofs_topological(V=V, entity_dim=1, entities=facets)
 
-# Now, we have specified the variational forms and can consider the
-# solution of the variational problem. First, we need to define a
-# :py:class:`Function <dolfin.functions.function.Function>` ``u`` to
-# represent the solution. (Upon initialization, it is simply set to the
-# zero function.) A :py:class:`Function
-# <dolfin.functions.function.Function>` represents a function living in
-# a finite element function space. Next, we can call the :py:func:`solve
-# <dolfin.fem.solving.solve>` function with the arguments ``a == L``,
-# ``u`` and ``bc`` as follows: ::
+# and use {py:func}`dirichletbc <dolfinx.fem.dirichletbc>` to create a
+# {py:class}`DirichletBCMetaClass <dolfinx.fem.DirichletBCMetaClass>`
+# class that represents the boundary condition
 
-# Compute solution
-u = Function(V)
-solve(a == L, u, bc)
+bc = fem.dirichletbc(value=ScalarType(0), dofs=dofs, V=V)
 
-# The function ``u`` will be modified during the call to solve. The
-# default settings for solving a variational problem have been
-# used. However, the solution process can be controlled in much more
-# detail if desired.
-# 
-# A :py:class:`Function <dolfin.functions.function.Function>` can be
-# manipulated in various ways, in particular, it can be plotted and
-# saved to file. Here, we output the solution to a ``VTK`` file (using
-# the suffix ``.pvd``) for later visualization and also plot it using
-# the :py:func:`plot <dolfin.common.plot.plot>` command: ::
+# Next, we express the variational problem using UFL.
 
-# Save solution in VTK format
-file = File("poisson.pvd")
-file << u
+# +
+u = ufl.TrialFunction(V)
+v = ufl.TestFunction(V)
+x = ufl.SpatialCoordinate(msh)
+f = 10 * ufl.exp(-((x[0] - 0.5) ** 2 + (x[1] - 0.5) ** 2) / 0.02)
+g = ufl.sin(5 * x[0])
+a = inner(grad(u), grad(v)) * dx
+L = inner(f, v) * dx + inner(g, v) * ds
+# -
 
-# Plot solution
-import matplotlib.pyplot as plt
-plot(u)
-plt.show()
+# We create a {py:class}`LinearProblem <dolfinx.fem.LinearProblem>`
+# object that brings together the variational problem, the Dirichlet
+# boundary condition, and which specifies the linear solver. In this
+# case we use a direct (LU) solver. The {py:func}`solve
+# <dolfinx.fem.LinearProblem.solve>` will compute a solution.
+
+# +
+problem = fem.petsc.LinearProblem(a, L, bcs=[bc], petsc_options={"ksp_type": "preonly", "pc_type": "lu"})
+uh = problem.solve()
+# -
+
+# The solution can be written to a  {py:class}`XDMFFile
+# <dolfinx.io.XDMFFile>` file visualization with ParaView or VisIt
+
+# +
+with io.XDMFFile(msh.comm, "out_poisson/poisson.xdmf", "w") as file:
+    file.write_mesh(msh)
+    file.write_function(uh)
+# -
